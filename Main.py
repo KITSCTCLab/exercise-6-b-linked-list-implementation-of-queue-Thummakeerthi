e1a99d97a8f0e912691afe8326c29016e54eef77
Thummakeerthi <110010517+Thummakeerthi@users.noreply.github.com>
2022-09-06 12:18:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,22 +1,32 @@
 class Node:
-  def __init__(self, data):
+  def _init_(self, data):
     self.data = data
     self.next = None
 
 
 class Queue:
-  def __init__(self):
+  def _init_(self):
     self.head = None
     self.last = None
 
   def enqueue(self, data) -> None:
     # Write your code here
+    new = Node(data)
 
   def dequeue(self) -> None:
     # Write your code here
+    if not self.head is None:
+      self.head = self.head.next
+      if self.head is None:
+        self.tail = None
+    
 
   def status(self) -> None:
     # Write your code here
+    if not self.head is None:
+      self.head = self.head.next
+      if self.head is None:
+        self.tail = None
 
 
 # Do not change the following code
